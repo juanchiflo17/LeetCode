a37baa99b8553d41cf08e2c598be3da207fd521f
juanchiflo17 <62221279+juanchiflo17@users.noreply.github.com>
2023-01-26 22:19:00 -0500

    Time: 20 ms (94.53%), Space: 24 MB (73.22%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> sol(n, 1);
+        int prefix = 1;
+        for (int i  = 0; i < n; i++) {
+            sol[i] = prefix;
+            prefix = prefix * nums[i];
+        }
+        int postfix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            sol[i] = sol[i] * postfix;
+            postfix = postfix * nums[i];
+        }
+
+        return sol;
+    }
+};
